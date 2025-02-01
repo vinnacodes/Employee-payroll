160f35c4de43aea672dcef4514b54070928328f9
vinnarasu <vinnarasu2000@gmail.com>
2025-02-01 22:35:23 +0530

    Employee servicce class creation

---
+++ src/main/java/com/vinna/Employee_payroll/service/EmployeeService.java
@@ -0,0 +1,24 @@
+package com.vinna.Employee_payroll.service;
+
+import com.vinna.Employee_payroll.model.Employee;
+import com.vinna.Employee_payroll.repo.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public Employee addEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    // Other service methods
+}
