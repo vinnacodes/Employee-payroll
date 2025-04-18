74855c2fc66d7b01c6a974f05585eb338a1b6350
vinnarasu <vinnarasu2000@gmail.com>
2025-04-18 09:15:24 +0530

    Update EmployeeService.java

--- src/main/java/com/vinna/Employee_payroll/service/EmployeeService.java
+++ src/main/java/com/vinna/Employee_payroll/service/EmployeeService.java
@@ -1,14 +1,15 @@
 package com.vinna.Employee_payroll.service;
 
 import com.vinna.Employee_payroll.model.Employee;
-import com.vinna.Employee_payroll.repo.EmployeeRepository;
+import com.vinna.Employee_payroll.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
+
     @Autowired
     private EmployeeRepository employeeRepository;
 
@@ -20,5 +21,24 @@ public class EmployeeService {
         return employeeRepository.findAll();
     }
 
-    // Other service methods
+    public Optional<Employee> getEmployeeById(Long id) {
+        return employeeRepository.findById(id);
+    }
+
+    // Updates employee if exists, else throws exception
+    public Employee updateEmployee(Long id, Employee employeeDetails) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Employee not found"));
+        employee.setName(employeeDetails.getName());
+        employee.setSalary(employeeDetails.getSalary());
+        employee.setDepartment(employeeDetails.getDepartment());
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(Long id) {
+        if (!employeeRepository.existsById(id)) {
+            throw new RuntimeException("Employee not found");
+        }
+        employeeRepository.deleteById(id);
+    }
 }
